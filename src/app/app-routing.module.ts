ae304ce1606d0a21797672241a6445abb7ae7ff1
Chiguripati <Gowtham.Chiguripati@McKesson.com>
2018-12-24 18:42:38 -0500

    CardsPayments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+ import { CardsComponent }   from './cards/cards.component';
+ import { PaymentsComponent } from './payments/payments.component';
+ // import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'payment', pathMatch: 'full' },
+    { path: 'cards', component: CardsComponent },
+   { path: 'payments', component: PaymentsComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
+
+/*
+Copyright 2017-2018 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { CardsComponent } from './cards/cards.component';
+import { PaymentsComponent } from './payments/payments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsComponent,
+    PaymentsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
